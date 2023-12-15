3b4c57d14a9773c7e9290c0387ed8ce8818a08f1
Daniil Lifanov <lifanovdaniil@gmail.com>
2023-12-15 22:34:31 +0400

    second commit

--- Lesson_2/Lesson_2.java
+++ Lesson_2/Lesson_2.java
@@ -2,6 +2,7 @@ package Lesson_2;
 
 public class Lesson_2 {
     public static void main(String[] args) {
+        printThreeWords();
         checkSumSign();
         printColor();
         compareNumbers();
