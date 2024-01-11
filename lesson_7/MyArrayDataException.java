91d5abf45bf8db165af7647bb2f7b9495fdd5107
Daniil Lifanov <lifanovdaniil@gmail.com>
2024-01-12 02:04:28 +0400

    lesson_7

--- lesson_7/MyArrayDataException.java
+++ lesson_7/MyArrayDataException.java
@@ -3,7 +3,6 @@ package lesson_7;
 public class MyArrayDataException extends NumberFormatException{
     public MyArrayDataException() {
     }
-
     public MyArrayDataException(String s) {
         super(s);
     }
