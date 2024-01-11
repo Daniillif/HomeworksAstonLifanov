372daacf2c72cf79fb517f75f9642a07865ae6e3
Daniil Lifanov <lifanovdaniil@gmail.com>
2024-01-12 01:07:03 +0400

    lesson_7

--- lesson_7/Lesson_7.java
+++ lesson_7/Lesson_7.java
@@ -6,19 +6,17 @@ import static lesson_7.Lesson_7.sumArray;
 
 public class Lesson_7 {
     public static void main(String[] args) throws MyArraySizeException {
-        String[][] arrayOfStrings = {{"3", "1", "1", "1"}, {"2", "2", "2", "2"}, {"1", "1", "1", "1"}, {"2", "2", "2", "2"}};
-        System.out.println(sumArray(arrayOfStrings));
+        String[][] arrayOfStrings = {{"3", "1", "1", "1"}, {"2", "2", "2", "7"}, {"5", "1", "1", "1"}, {"2", "2", "2", "2"}};
+        System.out.printf("Сумма передаваемого массива равна %d",sumArray(arrayOfStrings));
     }
     public static int sumArray(String[][] arrOfStrings) throws MyArraySizeException, MyArrayDataException {
         int count = 0;
         for (int i = 0; i < arrOfStrings.length; i++) {
-            for (int j = 0; j < arrOfStrings[i].length; j++) {
                 if (arrOfStrings[i].length == 4) {
                     count++;
-                    break;
                 } else
                     throw new MyArraySizeException("Количество элементов в массиве  " + i + "  не равно 4");
-            }
+
         }
         if (count != 4) {
             throw new MyArraySizeException("Размер массива не соответствует заявленным [4][4]");
@@ -28,11 +26,10 @@ public class Lesson_7 {
         for (int i = 0; i < arrOfStrings.length; i++) {
             for (int j = 0; j < arrOfStrings[i].length; j++) {
                 try {
-                    arrInt[i][j] = Integer.parseInt(arrOfStrings[i][j]);
-                    sumElements += arrInt[i][j];
+                    sumElements += Integer.parseInt(arrOfStrings[i][j]);
                 } catch (NumberFormatException ex) {
                     System.out.println(ex.getMessage());
-                    throw new MyArrayDataException("Не произошел парсинг в ячейке: " + "[" + i + "]" + "[" + j + "]");
+                    throw new MyArrayDataException(String.format("Не произошел парсинг в ячейке [%d][%d]",i,j));
                 }
             }
         }
