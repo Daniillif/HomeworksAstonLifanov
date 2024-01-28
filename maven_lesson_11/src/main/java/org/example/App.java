486769113ea9db6c00addf917142de67bb60f2ae
Daniil Lifanov <lifanovdaniil@gmail.com>
2024-01-28 20:14:57 +0400

    12 junit

--- maven_lesson_11/src/main/java/org/example/App.java
+++ maven_lesson_11/src/main/java/org/example/App.java
@@ -1,13 +1,24 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import java.util.Scanner;
+
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число");
+        System.out.println(getFactorial(scanner.nextInt()));
+
+    }
+    public static int getFactorial(int f) {
+        if (f <= 1) {
+            return 1;
+        }
+        else {
+            return f * getFactorial(f - 1);
+        }
     }
 }
