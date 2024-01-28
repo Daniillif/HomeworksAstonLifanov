fd5a1045348249dd9a6f4ebe7cbf1ec7fb2e63e7
Daniil Lifanov <lifanovdaniil@gmail.com>
2024-01-28 22:37:36 +0400

    12 junit

--- maven_lesson_11/src/main/java/org/example/App.java
+++ maven_lesson_11/src/main/java/org/example/App.java
@@ -2,22 +2,19 @@ package org.example;
 
 import java.util.Scanner;
 
-
-public class App
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число");
         System.out.println(getFactorial(scanner.nextInt()));
 
     }
+
     public static int getFactorial(int f) {
         if (f <= 1) {
             return 1;
-        }
-        else {
+        } else {
             return f * getFactorial(f - 1);
         }
     }
