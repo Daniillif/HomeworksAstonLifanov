345814894d36bcb7794ea69059bb43fe8af83206
Daniil Lifanov <lifanovdaniil@gmail.com>
2024-02-04 20:38:17 +0400

    merged 11 lesson into main

--- maven_lesson_11/src/main/java/org/example/App.java
+++ maven_lesson_11/src/main/java/org/example/App.java
@@ -2,19 +2,22 @@ package org.example;
 
 import java.util.Scanner;
 
-public class App {
-    public static void main(String[] args) {
+
+public class App 
+{
+    public static void main( String[] args )
+    {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число");
         System.out.println(getFactorial(scanner.nextInt()));
 
     }
-
     public static int getFactorial(int f) {
         if (f <= 1) {
             return 1;
-        } else {
+        }
+        else {
             return f * getFactorial(f - 1);
         }
     }
