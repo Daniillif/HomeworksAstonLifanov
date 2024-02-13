b0ecd42c1a6752a59d636d196e976c050aac1c01
Daniil Lifanov <lifanovdaniil@gmail.com>
2024-02-13 17:38:21 +0400

    lesson_15

--- maven/src/test/java/lesson_15/MainPage.java
+++ maven/src/test/java/lesson_15/MainPage.java
@@ -3,7 +3,9 @@ package lesson_15;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,7 +16,7 @@ public class MainPage {
     static List<ElementPage> listOfElements=  new ArrayList<>();
     static String btnToAdd = "//a[@class='product-card__add-basket j-add-to-basket btn-main']";
     static String lowestPrise = "//ins[@class='price__lower-price wallet-price']";
-    static String sizeL = "(//span[@class='sizes-list__size'])[1]";
+    static String sizeL = "//span[@class='sizes-list__size']";
 
     public MainPage() {
 
@@ -29,13 +31,15 @@ public class MainPage {
                 Thread.sleep(2000);
                 driver.findElement(By.xpath(btnToAdd)).click();
                 Thread.sleep(1000);
-                String name = driver.findElement(By.xpath("(//article[@data-card-index='0']//div[@class='product-card__wrapper']//div[@class='product-card__middle-wrap']//h2[@class='product-card__brand-wrap product-card__brand-wrap--no-wrap']//span[@class='product-card__name'])["+ i + "]")).getAttribute("textContent").replaceAll("[^A-Za-zА-Яа-я0-9]", "");
+                String name = driver.findElement(By.xpath("(//span[@class='product-card__name'])["+ i + "]")).getAttribute("textContent").replaceAll("[^A-Za-zА-Яа-я0-9]", "");
                 Thread.sleep(1000);
                 String prise = driver.findElement(By.xpath("("+lowestPrise+ ")["+ i + "]")).getAttribute("textContent").replaceAll("[^0-9]", "");
                 Thread.sleep(1000);
                 listOfElements.add(new ElementPage(name, prise));
                 Thread.sleep(1500);
-                driver.findElement(By.xpath(sizeL)).click();
+                WebElement elementOfSize = driver.findElement(By.xpath(sizeL));
+                Actions actions = new Actions(driver);
+                actions.moveToElement(elementOfSize).click().build().perform();
             } catch (NoSuchElementException exception) {
                 System.out.println("бузразмерный");
             } catch (InterruptedException e) {
